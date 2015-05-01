300fe67dba2c9b12e61bbe9002b95f339e5c7253
Nathan Rennie-Waldock <nathan.renniewaldock@gmail.com>
2015-05-01 20:35:03 +0100

    Remove old buildroot

--- buildroot-2012.02/package/kmod/kmod.mk
+++
@@ -1,7 +0,0 @@
-KMOD_VERSION = 5
-KMOD_SOURCE = kmod-$(KMOD_VERSION).tar.xz
-KMOD_SITE = http://packages.profusion.mobi/kmod/
-KMOD_INSTALL_STAGING = YES
-KMOD_DEPENDENCIES = host-pkg-config
-
-$(eval $(call AUTOTARGETS))
