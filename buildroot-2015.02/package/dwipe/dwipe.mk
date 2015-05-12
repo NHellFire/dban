0db599fba2a2ac387b07df3280fd8c4cc897ba7c
Nathan Rennie-Waldock <nathan.renniewaldock@gmail.com>
2015-05-12 12:47:33 +0100

    dwipe: Correct throughput formatting
    Also fetch from git, rather than tag archives just so
    we don't have to tag every single change

--- buildroot-2015.02/package/dwipe/dwipe.mk
+++ buildroot-2015.02/package/dwipe/dwipe.mk
@@ -1,56 +1,16 @@
-#############################################################
-#
-# dwipe
-#
-#############################################################
-DWIPE_VERSION:=2.1.1
-DWIPE_SOURCE:=v$(DWIPE_VERSION).tar.gz
-DWIPE_SITE:=https://github.com/NHellFire/dwipe/archive
-DWIPE_INSTALL_STAGING = YES
-DWIPE_INSTALL_TARGET = YES
-DWIPE_CONF_OPT =
+DWIPE_VERSION = a956dca389dcc9441e9ec212336bb2b443f33102
+DWIPE_SOURCE = dwipe-$(DWIPE_VERSION).tar.gz
+DWIPE_SITE = git://github.com/NHellFire/dwipe.git
 DWIPE_DEPENDENCIES = uclibc ncurses
-DWIPE_DIR:=$(BUILD_DIR)/dwipe-$(DWIPE_VERSION)
-DWIPE_BINARY:=dwipe
-DWIPE_TARGET_BINARY:=usr/local/bin/dwipe
-DWIPE_CAT:= $(ZCAT)
+DWIPE_BINARY = dwipe
 
-$(DL_DIR)/$(DWIPE_SOURCE):
-	$(WGET) -P $(DL_DIR) $(DWIPE_SITE)/$(DWIPE_SOURCE)
+define DWIPE_BUILD_CMDS
+	$(MAKE) GCC="$(TARGET_CC)" CC="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" -C $(@D) all
+endef
 
-dwipe-source: $(DL_DIR)/$(DWIPE_SOURCE)
-
-$(DWIPE_DIR)/.unpacked: $(DL_DIR)/$(DWIPE_SOURCE)
-	#cp -r dwipe-2.1.0 $(DWIPE_DIR)
-	$(DWIPE_CAT) $(DL_DIR)/$(DWIPE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -	
-	touch $(DWIPE_DIR)/.unpacked
-
-$(DWIPE_DIR)/.configured: $(DWIPE_DIR)/.unpacked
-	touch $@	
+define DWIPE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/$(DWIPE_BINARY) $(TARGET_DIR)/usr/local/bin/
+endef
 	
-#$(DWIPE_DIR)/$(DWIPE_BINARY): $(DWIPE_DIR)/.configured
-$(DWIPE_DIR)/$(DWIPE_BINARY): $(DWIPE_DIR)/.unpacked
-	$(MAKE) -C $(DWIPE_DIR) GCC="$(TARGET_CC)"
-	$(STRIPCMD) $(DWIPE_DIR)/$(DWIPE_BINARY)
-	
-$(TARGET_DIR)/$(DWIPE_TARGET_BINARY): $(DWIPE_DIR)/$(DWIPE_BINARY)
-	$(INSTALL) -m 0755 -D $(DWIPE_DIR)/$(DWIPE_BINARY) $(TARGET_DIR)/$(DWIPE_TARGET_BINARY)
-	#$(MAKE) DESTDIR=$(TARGET_DIR) -C $(DWIPE_DIR) install GCC="$(TARGET_CC)"
-	
-dwipe: uclibc $(TARGET_DIR)/$(DWIPE_TARGET_BINARY)
 
-dwipe-clean:
-	rm -f $(TARGET_DIR)/$(DWIPE_TARGET_BINARY)
-	-$(MAKE) -C $(DWIPE_DIR) clean
-
-dwipe-dirclean:
-	rm -rf $(DWIPE_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(strip $(BR2_PACKAGE_DWIPE)),y)
-TARGETS+=dwipe
-endif
+$(eval $(generic-package))
