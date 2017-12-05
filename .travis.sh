72f26b6a2374733f1e9c6ef10d5141fdb3645058
Nathan Rennie-Waldock <nathan.renniewaldock@gmail.com>
2017-12-05 01:28:47 +0000

    Hopefully get Travis working
    * Add missing dependencies
    * Build each architecture manually

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+ARCH="$1"
+if [ -z "$ARCH" ]; then
+	echo "Usage: $0 <arch>"
+	exit 1
+fi
+
 logfile=$(mktemp)
 while :; do printf .; sleep 30; done & pid=$!
 trap "cleanup" INT TERM EXIT
 cleanup() {
 	kill -TERM $pid
+	printf "\n"
 	tail -n 500 "$logfile"
 	rm -f "$logfile"
 }
 
-make > "$logfile" 2>&1
+make $ARCH > "$logfile" 2>&1
